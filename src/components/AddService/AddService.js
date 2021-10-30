734608ccc1785595361767c60489963690cb68ab
azizulwd <azizulislamgd@gmail.com>
2021-10-30 23:19:06 +0600

    for successfully created

--- src/components/AddService/AddService.js
+++ src/components/AddService/AddService.js
@@ -1,16 +1,26 @@
+import axios from 'axios';
 import React from 'react';
 import { useForm } from "react-hook-form";
 import './AddService.css';
 
 const AddService = () => {
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+        axios.post('http://localhost:5000/service', data)
+        .then(res =>{
+            if(res.data.insertedId){
+                alert('Added Successfully');
+                reset();
+            }
+        });
+    };
 
     return (
         <div className="add_service">
             <h1>Add Service</h1>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <input {...register("name", { required: true, maxLength: 20 })} placeholder="Service name" />
+                <input {...register("name")} placeholder="Service name" />
                 <textarea {...register("description")} placeholder="Description" />
                 <input type="number" {...register("price")} placeholder="Price" />
                 <input {...register("img")} placeholder="img url" />
