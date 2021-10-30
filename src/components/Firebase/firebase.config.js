fef0b99c4ec6b709b1975b73e7de435bdff92fcc
azizulwd <azizulislamgd@gmail.com>
2021-10-31 00:54:32 +0600

    firebase init done

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAJNG7KYkRq51cFs8qIWa2sXrAQl1ICuQI",
+    authDomain: "tixgig-f7d56.firebaseapp.com",
+    projectId: "tixgig-f7d56",
+    storageBucket: "tixgig-f7d56.appspot.com",
+    messagingSenderId: "992120553007",
+    appId: "1:992120553007:web:04488aba613598bf6890da"
+  };
+
+  export default firebaseConfig;
